15fef2a84448afda752ec879cab2947c722bcabb
Lowell Paige Bander <paige@lnxsrv06.seas.ucla.edu>
2014-04-11 21:41:50 -0700

    added hw1

---
+++ Homework1.c
@@ -0,0 +1,13 @@
+/*
+ *  * Generate mask indicating leftmost 1 in x.  Assume w=32.
+ *  * For example 0xFF00 -> 0x8000, and 0x6600 --> 0x4000.
+ *  * If x = 0, then return 0.
+ *   */
+int leftmost_one(unsigned x) {
+
+}
+
+
+int main() {
+
+}
