05ddfe4722162e2b16d2687ce361dc65d5acaac9
Lowell Bander <lowellbander@gmail.com>
2014-04-11 22:12:04 -0700

    added skeleton

--- Homework1.c
+++ Homework1.c
@@ -1,4 +1,6 @@
-/*
+// HOMEWORK 1: Problems 2.66, 2.71, 2.72, and 2.81 from the class text
+
+/* 2.66:
  *  * Generate mask indicating leftmost 1 in x.  Assume w=32.
  *  * For example 0xFF00 -> 0x8000, and 0x6600 --> 0x4000.
  *  * If x = 0, then return 0.
@@ -7,7 +9,28 @@ int leftmost_one(unsigned x) {
 
 }
 
+/* 2.71:
+ * Extract byte from word.  Return as signed integer */
+int xbyte(packed_t word, int bytenum) {
 
-int main() {
+// [WHY IS PREDECESSOR'S CODE WRONG?]
+}
+
+/* 2.72:
+ * Copy integer into buffer if space is available */
+void copy_int(int val, void *buf, int maxbytes) {
 
 }
+
+/* 2.81:
+ * A:
+ * B:
+ * C: 
+ * D:
+ * E:
+ * */
+
+int main() {
+	// TEST MAH FUNXIONS
+}
+
