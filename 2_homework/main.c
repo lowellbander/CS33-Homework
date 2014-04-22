391634cab142385a57a7b386a92134959685d1f1
Lowell Bander <lowellbander@gmail.com>
2014-04-21 17:03:42 -0700

    completed 3.56 part A

---
+++ 2_homework/main.c
@@ -0,0 +1,48 @@
+// Homework #2: 3.56 and 3.59 from the class text.
+
+#include <stdio.h>
+
+// 3.56
+int loop(int x, int n) {
+    // int result = _____;
+    // int mask;
+    // for (mask = _____; mask ￼_____ ; mask = ￼_____ ) { 
+    //   result ^= _____;
+    // }
+    // return result;
+
+    /*
+     * A)   %esi = x
+     *      %ebx = n
+     *      %edi = result
+     *      %eax = mask
+     *
+     * B)
+     *
+     * C)
+     *
+     * D)
+     *
+     * E)
+     *
+     * F)
+     * */
+
+
+}
+
+// 3.59
+int switch_prob(int x, int n) {
+    int result = x;
+
+    switch(n) {
+        /* Fill in code here */
+    }
+
+    return result;
+}
+
+int main() {
+    printf("%s\n", "hello world!");
+}
+
