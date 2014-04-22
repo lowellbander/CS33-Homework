51adf0de1049b7fdcb1ef878be72e0ab475c84ed
Lowell Bander <lowellbander@gmail.com>
2014-04-21 18:35:18 -0700

    switch skeleton created

--- 2_homework/main.c
+++ 2_homework/main.c
@@ -36,7 +36,24 @@ int switch_prob(int x, int n) {
     int result = x;
 
     switch(n) {
-        /* Fill in code here */
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        default:
+            x += 10;
+            break;
     }
 
     return result;
