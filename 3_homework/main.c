8a323f274f62f0f4c4c0f576273f4fd45768208b
Lowell Bander <lowellbander@gmail.com>
2014-05-05 17:11:29 -0700

    finished part A of first question

--- 3_homework/main.c
+++ 3_homework/main.c
@@ -1,4 +1,33 @@
-int main() {
+/* Homework #3: 3.64 and 3.67 from the class text
+ * Lowell Paige Bander
+ * UID 204 156 534
+ * */
 
+// 3.64
+/* A)   8(%ebp) == &s2
+ *      12(%ebp) == x
+ *      16(%ebp) == &y
+ *
+ * B)
+ *
+ * C) 
+ *
+ * D)
+ *
+ * */
+
+//3.67
+/* A) 
+ *
+ * B)
+ *
+ * C) (below)
+ * */
+void proc (union ele *up) {
+    up-> _____ = *(up-> ____) - up-> ____;
+}
+
+int main() {
+    printf("hello %s!\n", "world");
 }
 
